62d911ac6bc8fa3d7782e0ebdbe23e9b7fb4b02c
Olga Stoyakina <olyast1986@gmail.com>
2020-05-30 18:40:58 +0700

    Добавлен hello world

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+       
+        }
+    }
+}
